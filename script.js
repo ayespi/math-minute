b65128d48512e4d0f2508fb2c49fc2d1fbb300bd
RobertAKARobin <robertgfthomas@gmail.com>
2016-06-01 08:25:55 -0400

    Detecting correct answer and changing background color on guess

--- script.js
+++ script.js
@@ -5,6 +5,16 @@ $(document).on("ready", function(){
         $(".board").append(additionProblemUpTo(10));
         problemNumber += 1;
     }
+    $("input").on("change", function(){
+        var input = $(this);
+        var correctAnswer = input.attr("secret-answer");
+        var guess = input.val();
+        if(guess == correctAnswer){
+            input.css("background-color", "#afa");
+        }else{
+            input.css("background-color", "#faa");
+        }
+    });
 });
 
 function randomNumberUpTo(maximum){
@@ -15,5 +25,5 @@ function additionProblemUpTo(maximum){
     var firstNumber = randomNumberUpTo(maximum);
     var secondNumber = randomNumberUpTo(maximum);
     var sum = (firstNumber + secondNumber);
-    return ("<h2>" + firstNumber + " + " + secondNumber + " = " + "<input value='" + sum + "' />");
+    return ("<h2>" + firstNumber + " + " + secondNumber + " = " + "<input secret-answer='" + sum + "' value='' />");
 }
